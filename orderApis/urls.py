6282ee1d3c42e4458375fa12ded5f62ef0b289d8
darshitdave02 <darshitdave02@gmail.com>
2023-08-24 23:29:42 +0530

    fix: rename variables

--- orderApis/urls.py
+++ orderApis/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cheese', views.cheeseList, name='cheese-list'),
-    path('order', views.orderList, name='order-list'),
-    path('pizza', views.pizzaList, name='pizza-list'),
+    path('orders', views.orderList, name='order-list'),
+    path('pizzas', views.pizzaList, name='pizza-list'),
     path('pizzaBase', views.pizzaBaseList, name='pizzaBase-list'),
-    path('topping', views.toppingList, name='topping-list'),
+    path('toppings', views.toppingList, name='topping-list'),
     path('createOrder', views.orderCreate, name='order-create'),
     path('status/<int:order_id>', views.orderStatus, name='order-status'),
 
